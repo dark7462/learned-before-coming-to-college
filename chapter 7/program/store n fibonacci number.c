3881324266b0a29254f58f3291e5a057d0876f87
dark7462 <anu870906@gmail.com>
2024-09-12 09:42:51 +0530

    improved fibonacci

--- chapter 7/program/store n fibonacci number.c
+++ chapter 7/program/store n fibonacci number.c
@@ -10,11 +10,11 @@ int main(){
     fib[0]=0;
     fib[1]=1;
 
-    printf("%d \t %d \t",fib[0],fib[1]);
+    printf("%d \t %d \t",fib[0],fib[1]); //this will first print the const inital values 
 
     for(int i=2;i<n;i++){
         fib[i] = fib[i-1] + fib [i-2];
-        printf("%d \t",fib[i]);
+        printf("%d \t",fib[i]);         //this will then print the rest number of values which we wish for
     }
 
     return 0;
