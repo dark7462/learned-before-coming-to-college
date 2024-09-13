38bfd4f3eb3d6ec0a0843d73daf84168f8b12f66
dark7462 <anu870906@gmail.com>
2024-09-13 09:02:14 +0530

    updates

--- chapter 7/program/count odd number from array.c
+++ chapter 7/program/count odd number from array.c
@@ -2,7 +2,24 @@
 #include<math.h>
 
 //write a fuction to count the number of odd number in an array
+int countodd(int arr[],int n);
+
 
 int main(){
+
+    int odd[5]={1,2,3,4,5};
+
+    countodd(odd,5);
+
     return 0;
+}
+int countodd(int arr[],int n){
+    int count=0;
+    for (int i=0;i<=n;i++){
+        if(arr[i]%2!=0){
+            count++;
+        }
+    }
+    printf("%d",count);
+    return count;
 }
