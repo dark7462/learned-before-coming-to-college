7d7fcca9ec3f2099f1f1cc1ee18d1b8cc71a9b89
dark7462 <anu870906@gmail.com>
2024-09-14 21:03:45 +0530

    updated gst

--- chapter 5/learnings/gst.c
+++ chapter 5/learnings/gst.c
@@ -5,18 +5,22 @@ void gst(float value);
 
 int main(){
 
-    float value;
-    printf("enter tha value which you hav purchased the item->");
-    scanf("%f",&value);
+    // float value;
+    float gold;
+    gold=3663;
+    // printf("enter tha value which you hav purchased the item->");
+    // scanf("%f",&value);
 
-    printf("the initial value is->%f",value);
+    // printf("the initial value is->%f",value);
 
-    gst(value);
+    // gst(value);
+    gst(gold);
+
 
     return 0;
 }
 
 void gst(float value){
     value += (0.18*value);
-    printf("final price after including gst is : %f",value);
+    printf("final price after including gst is : %0.2f",value);
 }
